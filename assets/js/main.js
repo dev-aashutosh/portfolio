71dab763631bf675b9225b271b58dc1dd1eb3c93
ashutosh <dev.ashutoshh@gmail.com>
2023-12-15 21:22:23 +0530

    improved loader animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,5 +8,5 @@ $(document).ready(function () {
     // Hide the loader when the page is fully loaded
     // $('.pre-loader').fadeOut(3000);
     // If you want to show the loader for a specific duration (e.g., 2 seconds), use the delay method
-    $('.pre-loader').delay(2000).fadeOut(1000);
+    $('.pre-loader').delay(3000).fadeOut(100);
 });
