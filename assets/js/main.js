afa024ad0a8edc2410d4eadb8c1a2cb48b241d45
ashutosh <dev.ashutoshh@gmail.com>
2023-12-15 21:50:22 +0530

    added animation on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,4 @@
-// document.onreadystatechange = function () {
-//     if (document.readyState === "complete") {
-//         // Hide the loader when the page is fully loaded
-//         document.querySelector('.pre-loader').style.display = 'none';
-//     }
-// };
 $(document).ready(function () {
-    // Hide the loader when the page is fully loaded
-    // $('.pre-loader').fadeOut(3000);
-    // If you want to show the loader for a specific duration (e.g., 2 seconds), use the delay method
     $('.pre-loader').delay(3000).fadeOut(100);
+    AOS.init();
 });
